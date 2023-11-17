5fc5b66a21ea173a3f3051eb26342f1e1b721e11
Victor Simonsson <51890187+Vitoludde@users.noreply.github.com>
2023-11-17 23:13:40 +0100

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace BetterLC
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("LC_API", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInProcess("Lethal Company.exe")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
